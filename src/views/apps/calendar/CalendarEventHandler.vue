52a0c8978a79a7b06df46bbedfc59e4146165354
KelvinWong <kelvinwong65535@gmail.com>
2023-08-03 12:07:01 +0800

    improved page layout, new component created

--- src/views/apps/calendar/CalendarEventHandler.vue
+++ src/views/apps/calendar/CalendarEventHandler.vue
@@ -3,14 +3,14 @@ import type { Options } from 'flatpickr'
 import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
 import { VForm } from 'vuetify/components/VForm'
 
-import type { Event, NewEvent } from './types'
-import { useCalendarStore } from './useCalendarStore'
 import avatar1 from '@images/avatars/avatar-1.png'
 import avatar2 from '@images/avatars/avatar-2.png'
 import avatar3 from '@images/avatars/avatar-3.png'
 import avatar5 from '@images/avatars/avatar-5.png'
 import avatar6 from '@images/avatars/avatar-6.png'
 import avatar7 from '@images/avatars/avatar-7.png'
+import type { Event, NewEvent } from './types'
+import { useCalendarStore } from './useCalendarStore'
 
 import { requiredValidator, urlValidator } from '@validators'
 
@@ -123,7 +123,7 @@ const dialogModelValueUpdate = (val: boolean) => {
   >
     <!-- 👉 Header -->
     <AppDrawerHeaderSection
-      :title="event.id ? 'Update Event' : 'Add Event'"
+      :title="event? 'Update Event' : 'Add Event'"
       @cancel="$emit('update:isDrawerOpen', false)"
     >
       <template #beforeClose>
